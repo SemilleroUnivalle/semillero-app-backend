aff4a9a322f389a172a01b4f9ece033d9098d4c0
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-15 13:32:30 -0500

    Actualiazndo workflow 17

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,21 @@
 echo "Instalando dependencias antes del despliegue..."
 sudo apt update -y
 sudo apt install -y python3-pip python3-venv
+
+# Navegar al directorio de la aplicación
 cd /home/ubuntu/app
-python3 -m venv venv
+
+# Crear el entorno virtual solo si no existe
+if [ ! -d "venv" ]; then
+    echo "Creando el entorno virtual..."
+    python3 -m venv venv
+else
+    echo "El entorno virtual ya existe. Activándolo..."
+fi
+
+# Activar el entorno virtual
 source venv/bin/activate
+
+# Instalar dependencias
 pip install --upgrade pip
 pip install -r requirements.txt
