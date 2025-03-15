a765d81dc3c1b163b83a105f88508719512d8c47
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-15 13:39:53 -0500

    Actualiazndo workflow 18

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+set -e  # Detiene el script si hay un error
+
 echo "Instalando dependencias antes del despliegue..."
 sudo apt update -y
-sudo apt install -y python3-pip python3-venv
+sudo apt install -y python3-full python3-pip python3-venv
 
-# Navegar al directorio de la aplicación
-cd /home/ubuntu/app
-
-# Crear el entorno virtual solo si no existe
-if [ ! -d "venv" ]; then
-    echo "Creando el entorno virtual..."
-    python3 -m venv venv
-else
-    echo "El entorno virtual ya existe. Activándolo..."
+# Verificar si Python 3.10 está instalado y configurarlo como predeterminado
+if python3 --version | grep -q "Python 3.12"; then
+    echo "Cambiando a Python 3.10..."
+    sudo apt install -y python3.10 python3.10-venv python3.10-full python3.10-dev
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
 fi
 
-# Activar el entorno virtual
+# Crear carpeta si no existe
+mkdir -p /home/ubuntu/app
+
+cd /home/ubuntu/app
+
+# Crear entorno virtual si no existew
+if [ ! -d "venv" ]; then
+    echo "Creando entorno virtual..."
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
 
-# Instalar dependencias
-pip install --upgrade pip
+# Instalar dependencias dentro del entorno virtual
+pip install --upgrade pip --break-system-packages
 pip install -r requirements.txt
+
+echo "Before install completado."
