5a8b1ddc7832d7993afc44c654c843d122f667c1
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-15 13:21:30 -0500

    Actualiazndo workflow 16

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 echo "Instalando dependencias antes del despliegue..."
 sudo apt update -y
 sudo apt install -y python3-pip python3-venv
-cd /home/ubuntu/backend
+cd /home/ubuntu/app
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Iniciando aplicación en EC2..."
-cd /home/ubuntu/backend
+cd /home/ubuntu/app
 source venv/bin/activate
 python manage.py migrate
 python manage.py collectstatic --noinput
