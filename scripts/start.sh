f1a2fc346013e745035329209c21acd4805664b5
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-15 19:02:42 -0500

    Ajustando gunicorn para despliegue 1

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,10 +8,6 @@ source /home/ubuntu/app/venv/bin/activate
 # Moverse al directorio de la aplicación
 cd /home/ubuntu/app
 
-# Detener cualquier instancia previa de Gunicorn
-pkill -f gunicorn
-sleep 2
-
 # Iniciar Gunicorn con logs
 nohup gunicorn --workers 3 --bind 0.0.0.0:8000 project_semillero.wsgi:application > /home/ubuntu/app/gunicorn.log 2>&1 &
 
