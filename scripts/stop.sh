d55237aced160ae2d825aca29528d4a3cfea3957
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-15 19:05:09 -0500

    Ajustando gunicorn para despliegue 2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-pkill -f gunicorn
+
+echo "🛑 Deteniendo Gunicorn..."
+
+# Buscar si Gunicorn está corriendo
+if pgrep -f gunicorn > /dev/null
+then
+    pkill -f gunicorn
+    echo "✅ Gunicorn detenido exitosamente."
+else
+    echo "⚠️ No se encontró ningún proceso de Gunicorn en ejecución."
+fi
+
