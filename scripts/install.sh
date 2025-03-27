d9bd47bbd03a693f11c7de5d4efc449b44db619c
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-27 15:53:24 -0500

    retry deploy - appspec

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 cd /home/ubuntu/api/
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+sudo apt install -y python3.12-venv python3-pip 
+if [ ! -d "/home/ubuntu/api/venv" ]; then
+    python3 -m venv /home/ubuntu/api/venv
+fi
+
+# Activa el entorno virtual
+source /home/ubuntu/api/venv/bin/activate
+
+# Actualiza pip y setuptools dentro del entorno virtual
+pip install --upgrade pip setuptools
+
+# Instala dependencias del proyecto
+pip install -r /home/ubuntu/api/requirements.txt
+
+# Desactiva el entorno virtual
+deactivate
