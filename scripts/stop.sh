063e12079f5bdb85988e598b3f8b630a0d026dba
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-27 16:25:52 -0500

    retry deploy - appspec

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # Detener el proceso anterior (si está corriendo)
-
-# Verificar si el proceso está en ejecución antes de intentar terminarlo
-if pgrep -f manage.py > /dev/null; then
-    echo "Deteniendo proceso manage.py existente..."
-    pkill -f manage.py
-    echo "Proceso detenido correctamente."
-else
-    echo "No se encontró proceso manage.py en ejecución."
-fi
-
-# Asegurar que el script siempre termine con éxito
-exit 0
+pkill -f manage.py
