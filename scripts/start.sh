884897c2a5c378f019d305681d2e9dbbb5db0e5f
Sebas Tombe <johan.tombe@correounivalle.edu.co>
2025-03-27 15:53:24 -0500

    retry deploy - appspec

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/api/
-source venv/bin/activate
+source /home/ubuntu/api/venv/bin/activate
 # Cambia esto si usás Django, Flask, etc.
 # Ejemplo Django:
 nohup python manage.py runserver 0.0.0.0:8000 &
