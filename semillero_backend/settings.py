248d3c0fdd19ac3932d05c9f9d3db93404b2498c
Johan Sebastian Tombe Campo <80116536+SebasTombe@users.noreply.github.com>
2025-05-05 12:19:00 -0500

    Update settings.py

--- semillero_backend/settings.py
+++ semillero_backend/settings.py
@@ -135,16 +135,6 @@ WSGI_APPLICATION = 'semillero_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    """
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'semillero_database',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-    """
     # Configuración para usar SQLite
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
