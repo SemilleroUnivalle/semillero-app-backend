fdd5a18f461d2666acbad29233de056ed3210307
Johan Sebastian Tombe Campo <80116536+SebasTombe@users.noreply.github.com>
2025-09-11 21:54:56 -0500

    Clean up comments in settings.py

--- semillero_backend/settings.py
+++ semillero_backend/settings.py
@@ -140,7 +140,7 @@ WSGI_APPLICATION = 'semillero_backend.wsgi.application'
 
 # Base de datos
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-"""
+
 DATABASES = {
     # Configuración para usar SQLite
     'default': {
@@ -167,7 +167,7 @@ DATABASES = {
         'PORT': '5432',
     },
 }
-
+"""
 # Validación de contraseñas
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
