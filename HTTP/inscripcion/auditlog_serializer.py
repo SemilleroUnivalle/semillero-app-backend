e0b94434d7cad0301c38d4cefaecb2eff2fc57a8
Johan Sebastian Tombe Campo <80116536+SebasTombe@users.noreply.github.com>
2025-10-02 21:20:08 -0500

    Mostrando 3 datos en la auditoria id, usuario y tiempo

--- HTTP/inscripcion/auditlog_serializer.py
+++ HTTP/inscripcion/auditlog_serializer.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from auditlog.models import LogEntry
 
 class LogEntrySerializerEstudiante(serializers.ModelSerializer):
+    usuario = serializers.SerializerMethodField()
     class Meta:
         model = LogEntry
-        fields = '__all__' 
+        fields = ['timestamp', 'id', 'usuario']
+
+    def get_usuario(self, obj):
+        return obj.actor.first_name +" "+ obj.actor.last_name if obj.actor else None
--- HTTP/monitor_academico/auditlog_serializer.py
+++ HTTP/monitor_academico/auditlog_serializer.py
@@ -5,7 +5,7 @@ class LogEntrySerializerMonitorAcademico(serializers.ModelSerializer):
     usuario = serializers.SerializerMethodField()
     class Meta:
         model = LogEntry
-        fields = '__all__'
+        fields = ['timestamp', 'id', 'usuario']
 
     def get_usuario(self, obj):
         return obj.actor.first_name +" "+ obj.actor.last_name if obj.actor else None
